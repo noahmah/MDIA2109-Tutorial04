46520b18f68f71c9dc91e778a41db47b4cb14990
Noah Mah <noahmah@Noahs-MacBook-Pro.local>
2020-04-24 18:27:54 -0700

    new transitions

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import CustomButton from "../../comps/CustomButton";
 import Header from "../../comps/Header";
 import "./contact.css";
@@ -14,16 +14,44 @@ import { MdContacts } from "react-icons/Md";
 //   }, 1000);
 // }
 
+var index = 0;
+const headers = [
+  "Thanks for Contacting Me!",
+  "Goodbye",
+  "Let me know if you have other questions!",
+];
+
 const Contact = () => {
+  // Step One - Create the state variable, the function to update it and put a default inside.
+  const [header_text, setHeader] = useState("Contact Me!");
+  const [pageleft, setLeft] = useState("-100%");
+  // const [index, setIndex] = useState(0);
   useEffect(() => {
     setTimeout(() => {}, 50);
-    document.querySelector("#ContactApp").style.left = 0;
+    setLeft(0);
   }, []);
 
+  useEffect(() => {
+    setLeft(20);
+    setTimeout(() => {
+      setLeft(0);
+    }, 500);
+  }, [header_text]);
   return (
-    <div id="ContactApp">
-      <Header fontSize="24px" text="Contact Me!" />
-      <CustomButton text="email" />
+    // Step Two - Connect the state variable to the UI
+    <div id="ContactApp" style={{ left: pageleft }}>
+      <Header fontSize="24px" text={header_text} />
+      <CustomButton
+        text="email"
+        onClick={() => {
+          // Step Three - Connect the state function() to an interaction/figure out when you want to update the interface
+          setHeader(headers[index]);
+          index++;
+          if (index > headers.length - 1) {
+            index = 0;
+          }
+        }}
+      />
       <MdContacts fontSize="3em" />
       {/* <CustomButton onClick={GoHome} text="back to home" /> */}
       {/* <Link href="/../"></Link> */}
